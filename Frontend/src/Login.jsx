7d78ac682243b28d5ae70dd1b65b4820794af55f
gayathri-012 <gayathrikulal558@gmail.com>
2026-04-17 21:56:32 +0530

    sec-commit

--- Frontend/src/Login.jsx
+++ Frontend/src/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    axios.post("http://localhost:3001/login", {
+    axios.post("https://my-react-app-backend-4517.onrender.com/login", {
   email,
   password,
 })
