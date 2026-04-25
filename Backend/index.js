f9aa50de4138b33f596f7aaec791d63fca82a657
Gayathri <your-email@example.com>
2026-04-25 18:28:29 +0530

    fifth-commit

--- Backend/index.js
+++ Backend/index.js
@@ -11,19 +11,6 @@ const nodemailer = require("nodemailer");
 require("./models/Products");
 require("dotenv").config();
 
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "uploads/");
-//   },
-
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   }
-// });
-
-// const upload = multer({ storage: storage });
-
 const { v2: cloudinary } = require("cloudinary");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
